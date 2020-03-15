e2d4dfd0dd262712ace056a68e0557b997d0358d
gerardalmon <40595543+gerardalmon@users.noreply.github.com>
2020-03-15 19:07:15 +0200

    Update execCmdScript.sh

--- scripts/execCmdScript.sh
+++ scripts/execCmdScript.sh
@@ -16,21 +16,19 @@ if [ "${1}" == "master" ]; then
     sed -i "s|.*archive_mode.*|archive_mode = on|g" ${PGSQL_DATA}postgresql.conf;
     sed -i "s|.*archive_command.*| archive_command = 'cd .'|g" ${PGSQL_DATA}postgresql.conf;
     sudo /etc/init.d/postgresql-12 start
+    PGPASSWORD=${DB_PASSWORD} psql -Uwebadmin postgres -c "select pg_reload_conf();";
 fi
 
 if [ "${1}" == "slave" ]; then
     #set up slave
     sudo /etc/init.d/postgresql-12 stop
     rm -rf ${PGSQL_DATA};
-    PGPASSWORD=${DB_PASSWORD} pg_basebackup -h ${MASTER_IP} -D ${PGSQL_DATA} -U replication -v -P;
+    PGPASSWORD=${DB_PASSWORD} pg_basebackup -h ${MASTER_IP} -D ${PGSQL_DATA} -U replication -v -P -R;
     sed -i "1i host replication  replication       ${MASTER_IP}/32    trust" ${PGSQL_DATA}pg_hba.conf;
     sed -i "s|.*hot_standby.*|hot_standby = on|g" ${PGSQL_DATA}postgresql.conf;
     sed -i "153 a wal_level = hot_standby" ${PGSQL_DATA}postgresql.conf;
     sed -i "s|.*max_wal_senders.*|max_wal_senders = 1|g" ${PGSQL_DATA}postgresql.conf;
     sed -i "s|.*archive_mode.*|archive_mode = on|g" ${PGSQL_DATA}postgresql.conf;
     sed -i "s|.*archive_command.*| archive_command = 'cd .'|g" ${PGSQL_DATA}postgresql.conf;
-    echo "standby_mode = on" > ${PGSQL_DATA}recovery.conf;
-    echo "primary_conninfo = 'host=${MASTER_IP} port=5432 user=replication password=${DB_PASSWORD}'" >> ${PGSQL_DATA}recovery.conf;
-    echo "trigger_file = '/tmp/postgresql.trigger.5432'" >> ${PGSQL_DATA}recovery.conf;
     sudo /etc/init.d/postgresql-12 start
 fi
